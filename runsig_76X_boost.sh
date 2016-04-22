27c1a7e56ea9b1349e7a10b8a2f7d1479a2c8f09
asady1 <asady1@jhu.edu>
2016-04-21 22:04:48 -0400

    Create runsig_76X_boost.sh

---
+++ runsig_76X_boost.sh
@@ -0,0 +1,31 @@
+#/bin/sh
+
+#BG 1200
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-1200_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-1200_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_141815/0000/ --outName=BG_1200_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1200.txt &
+
+#BG 1400
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/user/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-1400_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160316_142930/0000/ --outName=BG_1400_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1400.txt &
+
+#BG 1600
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/user/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160316_142958/0000/ --outName=BG_1600_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1600.txt &
+
+#BG 1800
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/user/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-1800_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-1800_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_142027/0000/ --outName=BG_1800_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG1800.txt &
+
+#BG 2500
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_142200/0000/ --outName=BG_2500_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG2500.txt &
+
+#BG 3000
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/user/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160316_151510/0000/ --outName=BG_3000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG3000.txt &
+
+#BG 4000
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/user/lpchbb/HeppyNtuples/V21/BulkGravTohhTohbbhbb_narrow_M-4000_13TeV-madgraph/VHBB_HEPPY_V21_BulkGravTohhTohbbhbb_narrow_M-4000_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_142613/0000/ --outName=BG_4000_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/BG4000.txt &
+
+#R 1400
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21/RadionTohhTohbbhbb_narrow_M-1400_13TeV-madgraph/VHBB_HEPPY_V21_RadionTohhTohbbhbb_narrow_M-1400_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_145437/0000/ --outName=R_1400_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/R1400.txt &
+
+#R 1600
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21/RadionTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V21_RadionTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_145518/0000/ --outName=R_1600_76X_boost --trigger=False --jets=True --deta=True --isMC=True --xsec=1.0 --min=0 --max=1 --file=TxtFiles/R1600.txt &
+
+#R 3500
+python generalTreeAnalyzer_76X.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V21/RadionTohhTohbbhbb_narrow_M-3500_13TeV-madgraph/VHBB_HEPPY_V21_RadionTohhTohbbhbb_narrow_M-3500_13TeV-madgraph__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160408_145901/0000/ --outName=R_3500_76X_boost --trigger=False --jets=True --deta=True --isMC=True  --xsec=1.0 --min=0 --max=1 --file=TxtFiles/R3500.txt &
